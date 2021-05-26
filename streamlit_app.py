954c52b492d8fde30b4c40f75a5160cd64a747ec
oritsimu <76267927+oritsimu@users.noreply.github.com>
2021-05-26 20:15:46 +0100

    Update streamlit_app.py
    copy tweaks

--- streamlit_app.py
+++ streamlit_app.py
@@ -63,9 +63,9 @@ st.markdown('**Your 5 Top & Rising Google Trends Dashboard⚡**')
 linesDeduped2 = []
 MAX_LINES = 5
 text2 = st.markdown("Get your Top & Rising trends for 5 keywords, directly from Google Trends, no coding needed :sunglasses:.")
-text3 = st.markdown("To get started: Paste 1 keyword per line, pick your country (geo) & timeframe from the drop downs & hit 'Get Trends🤘' ")
+text3 = st.markdown("To get started: Paste 1 keyword per line, pick your country (geo) & timeframe from the dropdowns & hit 'Get Trends🤘' ")
 text = st.text_area("by Orit Mutznik (@oritsimu)", height=150, key=1)
-text2 = st.markdown('***Value column does not represent search volume, it is more of a value assigned by Google to signal how trending a kw is**')
+text2 = st.markdown('*Value column does not represent search volume, it is more of a value assigned by Google to signal how trending a kw is')
 lines = text.split("\n")  # A list of lines
 linesList = []
 for x in lines:
